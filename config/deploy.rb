94ab8abe8f375af35e053001ab74d56f3d6e4681
Bogdanov Anton <kortirso@gmail.com>
2015-10-24 21:00:51 +0300

    fix port

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '46.101.200.119', port: 3000, roles: [:web, :app, :db], primary: true
+server '46.101.200.119', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:kortirso/JRShop.git'
 set :application,     'JRShop'
