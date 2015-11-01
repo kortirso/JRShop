36677246d2b6b24f276d3d1109679451aab07a3b
Bogdanov Anton <kortirso@gmail.com>
2015-11-01 20:24:45 +0300

    delete commits

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,5 +1,3 @@
 class Manager < ActiveRecord::Base
-	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
