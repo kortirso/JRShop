9bdd14d7d7ce4bce237e10955df8b5ea910b3789
Bogdanov Anton <kortirso@gmail.com>
2015-11-10 20:05:00 +0300

    fix image uploading

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,7 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :small do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [150, 150]
   end
 
   def extension_white_list
